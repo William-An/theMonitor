fc3dc5f3e140d80be98e635ca8a82c3d2528e6f3
Weili An <China_Aisa@live.com>
2018-01-03 16:36:43 +0800

    Finish counter, add authentication for SZZX-WIFI

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import machine
 import network
 import time
+import urequests
 import umqtt.simple as mqtt
 import config
 
-# Handling interrupts
+# Handle interrupts
 # http://docs.micropython.org/en/latest/esp8266/reference/isr_rules.html
-class counter():
+
+class counter(object):
 	count = 0
 	def __init__(self,inp):
 		self.first = inp
@@ -16,19 +18,35 @@ class counter():
 			self.second = pin
 		self.first.irq(trigger=machine.Pin.IRQ_FALLING, handler=self.callback)
 	def callback(self,inp):
+		print('First: ',self.first)
+		print('Second: ',self.second)
+		print(self.__class__.count)
 		init_state = self.second.value()
 		start = time.ticks_ms()
 		while time.ticks_diff(time.ticks_ms(),start) < 5000:
 			if self.second.value() != init_state:
 				if self.second == pin:	# Leave
-					count = count - 1
+					self.__class__.count = self.__class__.count - 1
 				else:	# Enter
-					count = count + 1
-				mqclient.publish('libary',count)
+					self.__class__.count = self.__class__.count + 1
+				print(self.__class__.count)
+				mqclient.publish('libary',str(self.__class__.count))
+				print('Uploaded')
+				while self.second.value()!=init_state:
+					pass
+				break
+
+# Handle SZZX WIFI
+def szzx_connection():
+	res = urequests.request(config.SZZX_TEACHER_METHOD,\
+		config.SZZX_TEACHER_URL,\
+		data=config.SZZX_TEACHER_DATA,\
+		headers=config.SZZX_TEACHER_HEADER)
+	print('Connected')
+	res.close()
 
 # Led pin
 led = machine.Pin(config.LED_PIN, machine.Pin.OUT)
-# led = machine.Pin(2, machine.Pin.OUT)
 
 # Initializing Network
 # Need to consider SZZX
@@ -38,7 +56,11 @@ led.off()
 sta_if.connect(config.SSID, config.passwd)
 while not sta_if.isconnected():
 	pass
+if config.SSID
 led.on()
+
+# Connect to SMS WIFI
+szzx_connection()
 
 # Connect to server
 global mqclient
@@ -47,35 +69,18 @@ mqclient = mqtt.MQTTClient(config.MQTT_ID,config.MQTT_SERVER,\
 	ssl=config.MQTT_SSL)
 led.off()
 res = mqclient.connect()
+start = time.ticks_ms()
 while res is not 0:
 	res = mqclient.connect()
+	if (time.ticks_ms() - start) < 10000:
+		exit()
 led.on()
 
 # Initializing pins
 global pin
 pin = machine.Pin(config.IN_PIN, machine.Pin.IN)
 global pout
-pout = machine.Pin(config.OUT_PIN, machine.Pin.OUT)
+pout = machine.Pin(config.OUT_PIN, machine.Pin.IN)
 
-# pin.irq(trigger=machine.Pin.IRQ_FALLING, handler=enter)
-# pout.irq(trigger=machine.Pin.IRQ_FALLING, handler=leave)
 enter = counter(pin)
 leave = counter(pout)
-
-"""
-def enter(p):
-	# IRQ for people entering
-	init_state = pout.value()
-	start = time.tick_ms()
-	while time.tick_diff(time.tick_ms(),start) < 5000:
-		if pout.value() != init_state:
-			mqclient.publish('libary',1)
-
-def leave(p):
-	# IRQ for people leaving
-	init_state = pin.value()
-	start = time.tick_ms()
-	while time.tick_diff(time.tick_ms(),start) < 5000:
-		if pin.value() != init_state:
-			mqclient.publish('libary',-1)
-"""
